45ff8c5d609d37ee86c3eda0204e516c366b84bc
mfiloramo <mlfiloramo@gmail.com>
2023-03-14 20:12:54 -0400

    added websocket support

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+// MODULE IMPORTS
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import http from 'http';
+import WebSocket from 'ws';
 
+// ROUTE IMPORTS
+import { translationRouter } from "./routes/translationRouter";
 
 // GLOBAL VARIABLES
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ port: 8080 });
 const PORT = 3000;
 
 // CORS OPTIONS
@@ -25,11 +32,26 @@ app
 // ROUTE PROTECTION (PASSPORT, MSAL)
 // ...
 
-// APPLICATION ROUTE IMPORTS
-import { translationRouter } from "./routes/translationRouter";
-
 // APPLICATION ENDPOINTS
 app.use('/api', translationRouter);
+
+// WEBSOCKET SERVER
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+
+    ws.on('message', (message) => {
+        console.log(`Received message: ${JSON.stringify(message)}`);
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
 
 // RUN EXPRESS SERVER
 app.listen(PORT, () => {
