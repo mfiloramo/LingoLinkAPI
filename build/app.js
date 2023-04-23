10b2221266b6c2d6db48de5e4ea0065fea61ade3
mfiloramo <mlfiloramo@gmail.com>
2023-04-23 15:23:51 -0400

    Debug: Revert server

--- build/app.js
+++ build/app.js
@@ -1,1 +1,75 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// MODULE IMPORTS
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const ws_1 = __importDefault(require("ws"));
+// ROUTE IMPORTS
+const translationRouter_1 = require("./routes/translationRouter");
+const usersRouter_1 = require("./routes/usersRouter");
+const participantsRouter_1 = require("./routes/participantsRouter");
+const messagesRouter_1 = require("./routes/messagesRouter");
+const conversationsRouter_1 = require("./routes/conversationsRouter");
+const validateAccessToken_1 = require("./middleware/validateAccessToken");
+// GLOBAL VARIABLES
+const app = (0, express_1.default)();
+const wss = new ws_1.default.Server({ port: 8080 });
+const PORT = process.env.PORT || 3000;
+// CORS OPTIONS
+const corsOptions = {
+    origin: [process.env.CLIENT_URI],
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
+// APPLICATION DEPENDENCIES
+app
+    .use(express_1.default.json())
+    .use(express_1.default.urlencoded({ extended: false }))
+    .use(body_parser_1.default.urlencoded({ extended: true }))
+    .use((0, cors_1.default)(corsOptions));
+// ROUTES
+app.use('/api/translate', validateAccessToken_1.validateAccessToken, translationRouter_1.translationRouter);
+app.use('/api/users', validateAccessToken_1.validateAccessToken, usersRouter_1.usersRouter);
+app.use('/api/participants', validateAccessToken_1.validateAccessToken, participantsRouter_1.participantsRouter);
+app.use('/api/messages', validateAccessToken_1.validateAccessToken, messagesRouter_1.messagesRouter);
+app.use('/api/conversations', validateAccessToken_1.validateAccessToken, conversationsRouter_1.conversationsRouter);
+// HANDLE PREFLIGHT REQUESTS
+app.options('*', (0, cors_1.default)(corsOptions));
+// WILDCARD ENDPOINT
+app.use('*', (req, res) => {
+    res.status(404).send('Resource not found');
+});
+// WEBSOCKET SERVER
+wss.on('connection', (ws) => {
+    // SET CORS HEADERS
+    const headers = {
+        "Access-Control-Allow-Origin": process.env.CLIENT_URI,
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    };
+    Object.keys(headers).forEach((key) => {
+        ws.set(key, headers[key]);
+    });
+    // INDICATE CLIENT CONNECTION
+    console.log('Client connected...');
+    // BROADCAST WEBSOCKET DATA TO CLIENTS
+    ws.on('message', (message) => {
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === ws_1.default.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+    ws.on('close', () => {
+        // INDICATE CLIENT DISCONNECTION
+        console.log('Client disconnected...');
+    });
+});
+// RUN EXPRESS SERVER
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}...`);
+});
---
+++ src/app.ts
@@ -0,0 +1,82 @@
+// MODULE IMPORTS
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import WebSocket from 'ws';
+
+// ROUTE IMPORTS
+import { translationRouter } from "./routes/translationRouter";
+import { usersRouter } from "./routes/usersRouter";
+import { participantsRouter } from "./routes/participantsRouter";
+import { messagesRouter } from "./routes/messagesRouter";
+import { conversationsRouter } from "./routes/conversationsRouter";
+import { validateAccessToken } from './middleware/validateAccessToken';
+
+// GLOBAL VARIABLES
+const app = express();
+const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 3000;
+
+// CORS OPTIONS
+const corsOptions: any = {
+  origin: [process.env.CLIENT_URI],
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+// APPLICATION DEPENDENCIES
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(cors(corsOptions));
+
+// ROUTES
+app.use('/api/translate', validateAccessToken, translationRouter);
+app.use('/api/users', validateAccessToken, usersRouter);
+app.use('/api/participants', validateAccessToken, participantsRouter);
+app.use('/api/messages', validateAccessToken, messagesRouter);
+app.use('/api/conversations', validateAccessToken, conversationsRouter);
+
+// HANDLE PREFLIGHT REQUESTS
+app.options('*', cors(corsOptions));
+
+// WILDCARD ENDPOINT
+app.use('*', (req, res) => {
+  res.status(404).send('Resource not found');
+});
+
+// WEBSOCKET SERVER
+wss.on('connection', (ws: any) => {
+  // SET CORS HEADERS
+  const headers: any = {
+    "Access-Control-Allow-Origin": process.env.CLIENT_URI,
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+  };
+  Object.keys(headers).forEach((key) => {
+    ws.set(key, headers[key]);
+  });
+
+  // INDICATE CLIENT CONNECTION
+  console.log('Client connected...');
+
+  // BROADCAST WEBSOCKET DATA TO CLIENTS
+  ws.on('message', (message: any) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    // INDICATE CLIENT DISCONNECTION
+    console.log('Client disconnected...');
+  });
+});
+
+// RUN EXPRESS SERVER
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}...`);
+});
