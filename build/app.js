3a9b899d47c49c9799de0d5dac653acefa15bdeb
mfiloramo <mlfiloramo@gmail.com>
2023-04-23 11:57:34 -0400

    Change port to Azure defaults only

--- build/app.js
+++ build/app.js
@@ -18,7 +18,7 @@ const validateAccessToken_1 = require("./middleware/validateAccessToken");
 // GLOBAL VARIABLES
 const app = (0, express_1.default)();
 const wss = new ws_1.default.Server({ port: 8080 });
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 // CORS OPTIONS
 const corsOptions = {
     origin: ['http://localhost:4200', 'https://orange-tree-0d3c88e0f.3.azurestaticapps.net'],
--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ import { validateAccessToken } from './middleware/validateAccessToken';
 // GLOBAL VARIABLES
 const app = express();
 const wss = new WebSocket.Server({ port: 8080 });
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 // CORS OPTIONS
 const corsOptions: object = {
