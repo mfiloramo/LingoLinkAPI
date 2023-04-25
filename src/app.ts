9a9ae029d32b2c1c3b1a2e3049c1214b04f8d627
mfiloramo <mlfiloramo@gmail.com>
2023-04-25 01:12:30 -0400

    Revert to local use only

--- src/app.ts
+++ src/app.ts
@@ -35,11 +35,11 @@ app
 
 // TODO: ADD validateAccessToken MIDDLEWARE FOR EACH ROUTE
 // ROUTES
-app.use('/api/translate', validateAccessToken, translationRouter);
-app.use('/api/users', validateAccessToken, usersRouter);
-app.use('/api/participants', validateAccessToken, participantsRouter);
-app.use('/api/messages', validateAccessToken, messagesRouter);
-app.use('/api/conversations', validateAccessToken, conversationsRouter);
+app.use('/api/translate', translationRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/participants', participantsRouter);
+app.use('/api/messages', messagesRouter);
+app.use('/api/conversations', conversationsRouter);
 
 // HANDLE PREFLIGHT REQUESTS
 app.options('*', cors(corsOptions));
