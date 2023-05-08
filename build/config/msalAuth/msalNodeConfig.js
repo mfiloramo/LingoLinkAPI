057cea020989bcc9474edf2bab1b7af432a96dbb
mfiloramo <mlfiloramo@gmail.com>
2023-05-08 18:19:57 -0500

    Change pipeline script

--- build/config/msalAuth/msalNodeConfig.js
+++ build/config/msalAuth/msalNodeConfig.js
@@ -27,12 +27,12 @@ exports.API_ID_URI = exports.msalNodeConfig = void 0;
 const msal_node_1 = require("@azure/msal-node");
 const dotenv = __importStar(require("dotenv"));
 dotenv.config();
-const AZ_CLIENT_API = process.env['AZ_CLIENT_API'];
 const AZ_TENANT = process.env['AZ_TENANT'];
-const AZ_SECRET_VAL = process.env['AZ_SECRET_VAL'];
-const LOCAL_API_ID_URI = process.env['LOCAL_API_ID_URI'];
-const PROD_API_ID_URI = process.env['PROD_API_ID_URI'];
-// if (!AZ_TENANT || !AZ_SECRET_VAL || !LOCAL_API_ID_URI || !PROD_API_ID_URI) {
+const AZ_CLIENT_API_KEY = process.env['CLIENT_API_PROD'];
+const AZ_SECRET_API = process.env['AZ_SECRET_API'];
+const LOCAL_API_ID_URI = process.env['CLIENT_API_DEV'];
+const PROD_API_ID_URI = process.env['CLIENT_API_PROD'];
+// if (!AZ_TENANT || !AZ_SECRET_VAL) {
 //   throw new Error('AZ_TENANT, AZ_CLIENT_SECRET, LOCAL_API_ID_URI, and PROD_API_ID_URI must be defined in the environment variables.');
 // }
 function getApiIdUri() {
@@ -47,9 +47,9 @@ const API_ID_URI = getApiIdUri();
 exports.API_ID_URI = API_ID_URI;
 exports.msalNodeConfig = {
     auth: {
-        clientId: AZ_CLIENT_API,
+        clientId: AZ_CLIENT_API_KEY,
         authority: AZ_TENANT,
-        clientSecret: AZ_SECRET_VAL,
+        clientSecret: AZ_SECRET_API,
     },
     system: {
         loggerOptions: {
--- src/config/msalAuth/msalNodeConfig.ts
+++ src/config/msalAuth/msalNodeConfig.ts
@@ -1,15 +1,15 @@
-import { Configuration as NodeConfiguration, LogLevel } from '@azure/msal-node';
+import { LogLevel } from '@azure/msal-node';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const AZ_CLIENT_API = process.env['AZ_CLIENT_API'];
 const AZ_TENANT = process.env['AZ_TENANT'];
-const AZ_SECRET_VAL = process.env['AZ_SECRET_VAL'];
-const LOCAL_API_ID_URI = process.env['LOCAL_API_ID_URI'];
-const PROD_API_ID_URI = process.env['PROD_API_ID_URI'];
+const AZ_CLIENT_API_KEY = process.env['CLIENT_API_PROD'];
+const AZ_SECRET_API = process.env['AZ_SECRET_API'];
+const LOCAL_API_ID_URI = process.env['CLIENT_API_DEV'];
+const PROD_API_ID_URI = process.env['CLIENT_API_PROD'];
 
-// if (!AZ_TENANT || !AZ_SECRET_VAL || !LOCAL_API_ID_URI || !PROD_API_ID_URI) {
+// if (!AZ_TENANT || !AZ_SECRET_VAL) {
 //   throw new Error('AZ_TENANT, AZ_CLIENT_SECRET, LOCAL_API_ID_URI, and PROD_API_ID_URI must be defined in the environment variables.');
 // }
 
@@ -23,11 +23,11 @@ function getApiIdUri() {
 
 const API_ID_URI = getApiIdUri();
 
-export const msalNodeConfig: NodeConfiguration = {
+export const msalNodeConfig: any = {
   auth: {
-    clientId: AZ_CLIENT_API!,
+    clientId: AZ_CLIENT_API_KEY,
     authority: AZ_TENANT,
-    clientSecret: AZ_SECRET_VAL,
+    clientSecret: AZ_SECRET_API,
   },
   system: {
     loggerOptions: {
