84973461f5312c39ad8c290250a88daf71320ed7
mfiloramo <mlfiloramo@gmail.com>
2023-05-17 09:55:35 -0400

    Pipeline fix attempt

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'app.ts'), // Update entry path
+  entry: './src/app.ts',
+  target: 'node',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'commonjs2',
   },
   externals: [nodeExternals()],
-  target: 'node',
   module: {
     rules: [
       {
