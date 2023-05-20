132cdac7c4d8bebdf2f2d412ca211faa01b77fbb
mfiloramo <mlfiloramo@gmail.com>
2023-05-20 15:43:15 -0400

    Debug: Disable routers/middleware

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import cors, { CorsOptions } from 'cors';
 import WebSocket from 'ws';
 
 // ROUTE IMPORTS
-import { translationRouter } from './routes/translationRouter';
-import { usersRouter } from './routes/usersRouter';
-import { participantsRouter } from './routes/participantsRouter';
-import { messagesRouter } from './routes/messagesRouter';
-import { conversationsRouter } from './routes/conversationsRouter';
-import { validateAccessToken } from './middleware/validateAccessToken';
+// import { translationRouter } from './routes/translationRouter';
+// import { usersRouter } from './routes/usersRouter';
+// import { participantsRouter } from './routes/participantsRouter';
+// import { messagesRouter } from './routes/messagesRouter';
+// import { conversationsRouter } from './routes/conversationsRouter';
+// import { validateAccessToken } from './middleware/validateAccessToken';
 
 // GLOBAL VARIABLES
 const app = express();
@@ -33,42 +33,40 @@ app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(cors(corsOptions));
 
-// SERVER ROUTES
-app.use('/api/translate', validateAccessToken, translationRouter);
-app.use('/api/users', validateAccessToken, usersRouter);
-app.use('/api/participants', validateAccessToken, participantsRouter);
-app.use('/api/messages', validateAccessToken, messagesRouter);
-app.use('/api/conversations', validateAccessToken, conversationsRouter);
-
-// HANDLE PREFLIGHT REQUESTS
+// // SERVER ROUTES
+// app.use('/api/translate', validateAccessToken, translationRouter);
+// app.use('/api/users', validateAccessToken, usersRouter);
+// app.use('/api/participants', validateAccessToken, participantsRouter);
+// app.use('/api/messages', validateAccessToken, messagesRouter);
+// app.use('/api/conversations', validateAccessToken, conversationsRouter);
+//
+// // HANDLE PREFLIGHT REQUESTS
 app.options('*', cors(corsOptions));
-
-// WILDCARD ENDPOINT
-app.use('*', (req, res) => {
-  res.status(404).send('Resource not found');
-});
-
-// WEBSOCKET SERVER
-wss.on('connection', (ws: WebSocket) => {
-  // INDICATE CLIENT CONNECTION
-  console.log('Client connected...');
-
-  // BROADCAST WEBSOCKET DATA TO CLIENTS
-  ws.on('message', (message: WebSocket.Data) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    // INDICATE CLIENT DISCONNECTION
-    console.log('Client disconnected...');
-  });
-});
-
-console.log('tomato');
+//
+// // WILDCARD ENDPOINT
+// app.use('*', (req, res) => {
+//   res.status(404).send('Resource not found');
+// });
+//
+// // WEBSOCKET SERVER
+// wss.on('connection', (ws: WebSocket) => {
+//   // INDICATE CLIENT CONNECTION
+//   console.log('Client connected...');
+//
+//   // BROADCAST WEBSOCKET DATA TO CLIENTS
+//   ws.on('message', (message: WebSocket.Data) => {
+//     wss.clients.forEach((client) => {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         client.send(message);
+//       }
+//     });
+//   });
+//
+//   ws.on('close', () => {
+//     // INDICATE CLIENT DISCONNECTION
+//     console.log('Client disconnected...');
+//   });
+// });
 
 // RUN EXPRESS SERVER
 app.listen(PORT, () => {
