801f9161119c929dbf55be04ce2b0c221b108542
mfiloramo <mlfiloramo@gmail.com>
2023-10-17 18:34:47 -0400

    Refactor user validation

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,26 +3,43 @@ import { wcCoreMSQLConnection } from "../config/database/wcCoreMSQLConnection";
 import jwt from 'jsonwebtoken';
 
 
-export const usersController = async (req: Request, res: Response) => {
+export const usersController = async (req: Request, res: Response): Promise<void> => {
   switch (req.method) {
     case 'GET':
-      if (!req.body.userId) {
+      if (!req.body.userId && !req.query) {
         // SELECT ALL USERS
         try {
           const selectAll = await wcCoreMSQLConnection.query('EXECUTE usp_User_SelectAll')
-          res.send(selectAll[0][0]); // TODO: CHANGE BACK TO ARRAY (REMOVE 1 [0])
+          res.send(selectAll[0]); // TODO: CHANGE BACK TO ARRAY (REMOVE 1 [0])
         } catch (error: any) {
           res.status(500).send(error);
         }
-      } else {
-        // SELECT USER BY ID
+      } else if (req.query) {
+        console.log('debug')
+        // VALIDATE USER AGAINST DATABASE
         try {
-          const response = await wcCoreMSQLConnection.query('EXECUTE usp_User_Select :userId', {
+          const response = await wcCoreMSQLConnection.query
+          ('EXECUTE usp_User_Validate :email, :password', {
             replacements: {
-              userId: req.body.userId,
+              email: req.query.email,
+              password: req.query.password
             }
           })
-          res.send(response[0][0]);
+          res.send(response[0][0])
+        } catch (error: any) {
+          res.status(500).send(error);
+          console.log(error);
+        }
+      }
+        else {
+          // SELECT USER BY ID
+          try {
+            const response = await wcCoreMSQLConnection.query('EXECUTE usp_User_Select :userId', {
+              replacements: {
+                userId: req.body.userId,
+              }
+            })
+            res.send(response[0][0]);
         } catch (error: any) {
           res.status(500).send(error);
           console.log(error);
